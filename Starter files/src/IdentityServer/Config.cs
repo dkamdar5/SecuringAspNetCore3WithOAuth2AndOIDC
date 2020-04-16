14e9497fb59398f50ee7126b6f9a19b44075e372
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2020-04-16 11:30:31 -0500

    adding Api and Client projects

--- Starter files/src/IdentityServer/Config.cs
+++ Starter files/src/IdentityServer/Config.cs
@@ -16,12 +16,31 @@ namespace IdentityServer
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
+            new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
         
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
         
     }
 }
