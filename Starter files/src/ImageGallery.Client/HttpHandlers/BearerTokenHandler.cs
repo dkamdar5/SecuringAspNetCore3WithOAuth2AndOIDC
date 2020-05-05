b0bd64e1ec9459792565c0f3b88dd24b7e6ca24f
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2020-05-05 11:23:26 -0500

    using refresh token to gain long lived access

--- Starter files/src/ImageGallery.Client/HttpHandlers/BearerTokenHandler.cs
+++ Starter files/src/ImageGallery.Client/HttpHandlers/BearerTokenHandler.cs
@@ -1,8 +1,11 @@
 using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,11 +15,16 @@ namespace ImageGallery.Client.HttpHandlers
     public class BearerTokenHandler : DelegatingHandler
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public BearerTokenHandler(IHttpContextAccessor httpContextAccessor)
+        public BearerTokenHandler(
+            IHttpContextAccessor httpContextAccessor,
+            IHttpClientFactory httpClientFactory)
         {
             _httpContextAccessor = httpContextAccessor ??
                 throw new ArgumentNullException(nameof(httpContextAccessor));
+            _httpClientFactory = httpClientFactory ??
+                throw new ArgumentNullException(nameof(httpClientFactory));
         }
 
 
@@ -25,11 +33,7 @@ namespace ImageGallery.Client.HttpHandlers
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            var accessToken = await _httpContextAccessor
-                .HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
-
-            var refreshToken = await _httpContextAccessor
-                .HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            var accessToken = await GetAccessTokenAsync();
 
             if (!string.IsNullOrWhiteSpace(accessToken))
             {
@@ -37,6 +41,80 @@ namespace ImageGallery.Client.HttpHandlers
             }
 
             return await base.SendAsync(request, cancellationToken);
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var expiresAt = await _httpContextAccessor
+                .HttpContext.GetTokenAsync("expires_at");
+
+            var expiresAtAsDateTimeOffset = 
+                DateTimeOffset.Parse(expiresAt, CultureInfo.InvariantCulture);
+
+            if ((expiresAtAsDateTimeOffset.AddSeconds(-60)).ToUniversalTime() > DateTime.UtcNow)
+            {
+                // no need to refresh, return the access token
+                return await _httpContextAccessor
+                    .HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            }
+
+            var idpClient = _httpClientFactory.CreateClient("IDPClient");
+
+            //get discovery document
+            var discoveryResponse = await idpClient.GetDiscoveryDocumentAsync();
+
+            var refreshToken = await _httpContextAccessor
+                .HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            
+            var refreshResponse = await idpClient.RequestRefreshTokenAsync(
+                new RefreshTokenRequest
+                {
+                    Address = discoveryResponse.TokenEndpoint,
+                    ClientId = "imagegalleryclient",
+                    ClientSecret = "secret",
+                    RefreshToken = refreshToken
+                }
+            );
+
+            // store tokens
+            var updatedTokens = new List<AuthenticationToken>();
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.IdToken,
+                Value = refreshResponse.IdentityToken
+            });
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.AccessToken,
+                Value = refreshResponse.AccessToken
+            });
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.RefreshToken,
+                Value = refreshResponse.RefreshToken
+            });
+            updatedTokens.Add(new AuthenticationToken
+            {
+                Name = "expires_at",
+                Value = (DateTime.UtcNow + TimeSpan.FromSeconds(refreshResponse.ExpiresIn))
+                    .ToString("o", CultureInfo.InvariantCulture)
+            });
+
+            // get authenticate result, containing the current principal & 
+            // properties
+            var currentAuthenticateResult = await _httpContextAccessor
+                .HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // store the updated tokens
+            currentAuthenticateResult.Properties.StoreTokens(updatedTokens);
+
+            // sign in
+            await _httpContextAccessor.HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                currentAuthenticateResult.Principal,
+                currentAuthenticateResult.Properties);
+
+            return refreshResponse.AccessToken;
         }
     }
 }
